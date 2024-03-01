0ccf819b57cadf6e70693962b2fc2c66588a6a6f
obaraelijah <elijahobara357@gmail.com>
2024-03-01 10:16:18 +0300

    feat: add more website routes

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,11 @@
-use rocket::{Route, Catcher};
 use rocket_contrib::templates::Template;
+use rocket_contrib::serve::StaticFiles;
+use rocket::response::NamedFile;
+use rocket::http::Status;
 
 use crate::context::{get_base_context, get_template};
+use rocket::{http, Request};
+use rocket::{Route, Catcher};
 
 
 #[get("/")]
@@ -11,11 +15,61 @@ fn index() -> Template {
     Template::render(get_template("/"), context)
 }
 
+#[get("/resume")]
+fn resume() -> Template {
+    let mut context = get_base_context("/resume");
+    context.kv.insert("title".to_owned(), "resume".into());
+    Template::render(get_template("/resume"), context)
+}
+
+#[get("/blog")]
+fn blog_index() -> Template {
+    let mut context = get_base_context("/blog");
+    context.kv.insert("title".to_owned(), "blog".into());
+    Template::render(get_template("/blog"), context)
+}
+
+#[get("/linkedin")]
+fn linkedin() -> Template {
+    let mut context = get_base_context("/linkedin");
+    context.kv.insert("title".to_owned(), "linkedin".into());
+    Template::render(get_template("/linkedin"), context)
+}
+
+#[get("/github")]
+fn github() -> Template {
+    let mut context = get_base_context("/github");
+    context.kv.insert("title".to_owned(), "github".into());
+    Template::render(get_template("/github"), context)
+}
+
+#[catch(404)]
+fn not_found(req: &Request<'_>) -> Template {
+    let mut context = get_base_context("/");
+    context.kv.insert("uri".to_owned(), req.uri().to_string());
+    context.kv.insert("title".to_owned(), "404".to_owned());
+    Template::render(get_template("404"), context)
+}
+
+#[catch(500)]
+fn server_err(req: &Request<'_>) -> Template {
+    let mut context = get_base_context("/");
+    context.kv.insert("uri".to_owned(), req.uri().to_string());
+    context.kv.insert("title".to_owned(), "500".to_owned());
+    Template::render(get_template("500"), context)
+}
+
+
+
 pub fn get_routes() -> ( Vec<Route>, Vec<Catcher>) {
     (
         routes![
             index,
+            resume,
+            blog_index,
+            linkedin,
+            github,
         ],
-        catchers![],
+        catchers![not_found, server_err],
     )
 }
