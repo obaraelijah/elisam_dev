d4e995ddb87789d764b2f049809cb585210e5865
obaraelijah <elijahobara357@gmail.com>
2024-03-01 13:00:25 +0300

    test: fix test issue and main

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@ extern crate rocket;
 extern crate log;
 
 #[cfg(test)]
-mod test;
+mod tests;
 
 mod server;
 mod routes;
 mod context;
 mod blog;
 
+use context::init_context;
 use server::start_server;
+
 
 fn main() {
     simple_logger::SimpleLogger::new()
         .with_level(log::LevelFilter::Info)
         .init()
         .unwrap();
+    init_context();
     start_server().launch();
 } 
--- src/tests.rs
+++ src/tests.rs
@@ -16,12 +16,12 @@ mod test {
         let client = get_client();
         for route in routes {
             let resp = client.get(route).dispatch();
-            assert_eq!(resp.Status(), Status.Ok());
+            assert_eq!(resp.status(), Status::Ok);
         }
     }
 
     #[test]
-    fn unknown_pages_should_404() {
+    fn unknown_pages_should_return_404() {
         let routes = vec!["/adawdawd", "/ada2wdawd/adawdw", "/ada3dawd/afwf/afwafa"];
         let client = get_client();
         for route in routes {
@@ -32,7 +32,7 @@ mod test {
     }
 
     #[test]
-    fn five_hundred_page_should_500() {
+    fn five_hundred_page_should_return_500() {
         let client = get_client();
         let resp = client.get("/500").dispatch();
         assert_eq!(resp.status(), Status::InternalServerError);
