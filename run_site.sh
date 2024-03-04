ce4829258e1c0e7ed58cd9e9abddf28b1bc59e94
obaraelijah <elijahobara357@gmail.com>
2024-03-04 17:57:59 +0300

    prod: Add make_package.sh

---
+++ run_site.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EXISTING_INSTANCE="$(pidof target/release/elisam_dev)"
+
+if [ ! -z $EXISTING_INSTANCE ]; then
+    kill $EXISTING_INSTANCE
+fi
+
+BIN_NAME="elisam_dev"
+
+if [ -f "elisam_dev" ]; then
+    ./$BIN_NAME
+    exit $?
+fi
+
+if [ -f "Cargo.toml" ]; then
+   cargo run --release
+   exit $?
+fi
+
+echo "Could not find elisam_dev executable or Cargo.toml"
